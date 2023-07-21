8bd5e61902822c4b8fd9dcda1977a4e4a8d02616
Sunny Srivastava <sunnsr25@in.ibm.com>
2023-07-21 01:58:43 -0500

    APIs to fill VPD map and set JSON
    The commit implements APIs to call respective parser and fill
    parsed VPD map.
    It also implements API to fetch IM and HW value required to pick
    correct JSON for the system.
    
    Signed-off-by: Sunny Srivastava <sunnsr25@in.ibm.com>

--- include/ipz_parser.hpp
+++ include/ipz_parser.hpp
@@ -64,7 +64,7 @@ class IpzVpdParser : public ParserInterface
      *
      * @return parsed VPD data.
      */
-    std::variant<types::ParsedVPD, types::KeywordVpdMap> parse();
+    types::VPDMapVariant parse();
 
     /**
      * @brief API to check validity of VPD header.
